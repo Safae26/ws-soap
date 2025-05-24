5f2516f93cc4e7baf25640f0ad59f6ccaef3daa3
Safae <149904770+safae26@users.noreply.github.com>
2025-05-24 17:30:55 +0100

    Update README.md

---
+++ client-soap-java/src/test/java/net/safae/clientsoapjava/ClientSoapJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.net.safae.clientsoapjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSoapJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
